1b3a20ae69858e88ebf8b5a7f19f47c9f9ffc788
XzoRit <xzorit@hotmail.com>
2020-09-19 15:59:30 +0200

    integrate ftxui lib into build files

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,13 @@
 #include <lib/lib.hpp>
 
+#include <ftxui/component/menu.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+
 #include <boost/program_options.hpp>
 
 #include <iostream>
+
+using namespace ftxui;
 
 namespace po = boost::program_options;
 
@@ -33,6 +38,17 @@ int main(int ac, char* av[])
     {
         std::cerr << "Exception of unknown type!\n";
     }
+
+    auto screen = ScreenInteractive::TerminalOutput();
+
+    Menu menu;
+    menu.entries = {L"entry 1", L"entry 2", L"entry 3"};
+    menu.selected = 0;
+    menu.on_enter = screen.ExitLoopClosure();
+
+    screen.Loop(&menu);
+
+    std::cout << "Selected element = " << menu.selected << std::endl;
 
     return xzr::lib::add(0, 0);
 }
